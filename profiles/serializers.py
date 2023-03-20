8900fd138a4cc5f1449773f20ad50980571059bc
Bogdan <bogdan.gugu93@gmail.com>
2023-03-20 16:44:53 +0000

    Add extra view data for likes and followers for profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,11 +2,13 @@ from rest_framework import serializers
 from .models import Profile
 from followers.models import Follower
 
-
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -18,6 +20,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             following = Follower.objects.filter(
                 owner=user, followed=obj.owner
             ).first()
+            # print(following)
             return following.id if following else None
         return None
 
@@ -26,4 +29,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
             'content', 'image', 'is_owner', 'following_id',
+            'posts_count', 'followers_count', 'following_count',
         ]
