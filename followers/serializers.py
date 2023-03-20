46cd191b9b61f1189406418fd4906cc69a439ac4
Bogdan <bogdan.gugu93@gmail.com>
2023-03-20 13:58:23 +0000

    Create and add followerserializer and create integrity error

---
+++ followers/serializers.py
@@ -0,0 +1,22 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Follower
+
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Follower
+        fields = [
+            'id', 'owner', 'created_at', 'followed', 'followed_name'
+        ]
+    
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IndentationError:
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
