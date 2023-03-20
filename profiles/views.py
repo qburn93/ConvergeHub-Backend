3306ec261f7275a666c61c201c6c8134bce38fac
Bogdan <bogdan.gugu93@gmail.com>
2023-03-20 14:18:21 +0000

    Fixed:forgot generics apiview and retrive destroy api view in profile app

--- profiles/views.py
+++ profiles/views.py
@@ -4,13 +4,13 @@ from .models import Profile
 from .serializers import ProfileSerializer
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListCreateAPIView):
 
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
     permission_classes = [IsOwnerOrReadOnly]
