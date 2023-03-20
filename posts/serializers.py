020aeae500f858b9843eb10b44a5c0ac83d82c65
Bogdan <bogdan.gugu93@gmail.com>
2023-03-20 18:11:09 +0000

    Bugg: cant run server due to no module named django filters, did pip uninstall django-filters and installed djangoframeworks_filters

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,16 +1,12 @@
-from rest_framework import serializers
-from posts.models import Post
-from likes.models import Like
-
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
-    comments_count = serializers.ReadOnlyField()
     likes_count = serializers.ReadOnlyField()
-    
+    comments_count = serializers.ReadOnlyField()
+
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
@@ -32,10 +28,10 @@ class PostSerializer(serializers.ModelSerializer):
         user = self.context['request'].user
         if user.is_authenticated:
             like = Like.objects.filter(
-                owner=user, post=obj).first()
+                owner=user, post=obj
+            ).first()
             return like.id if like else None
         return None
-        
 
     class Meta:
         model = Post
@@ -43,6 +39,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter',
-            'like_id', 'comments_count', 'likes_count',
-            
+            'like_id', 'likes_count', 'comments_count',
         ]
