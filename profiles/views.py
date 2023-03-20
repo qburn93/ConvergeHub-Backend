88824a8a65c958fc95585eb8350b99c983b0d90c
Bogdan <bogdan.gugu93@gmail.com>
2023-03-20 16:21:15 +0000

    Added extra fileds to profile and post serializers with like id and follower id

--- profiles/views.py
+++ profiles/views.py
@@ -4,13 +4,19 @@ from .models import Profile
 from .serializers import ProfileSerializer
 
 
-class ProfileList(generics.ListCreateAPIView):
-
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by django signals.
+    """
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 
-class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
