efb62aa5b53a06c947b40a326e6feec8cbb4d081
Bogdan <bogdan.gugu93@gmail.com>
2023-03-25 11:45:45 +0000

    Setup post for category

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,14 @@
 from django.db.models import Count
-from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
-from convergehub_api.permissions import IsOwnerOrReadOnly
-from .models import Post
+from rest_framework import filters, generics, permissions, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
 from categories.models import Category
-from .serializers import PostSerializer, CategorySerializer
+from convergehub_api.permissions import IsOwnerOrReadOnly
+
+from .models import Post
+from .serializers import CategorySerializer, PostSerializer
 
 
 class CategoryList(generics.ListCreateAPIView):
@@ -12,13 +16,15 @@ class CategoryList(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
 
 
-class PostList(generics.ListCreateAPIView):
+class PostList(generics.GenericAPIView):
     """
     List posts or create a post if logged in
     The perform_create method associates the post with the logged in user.
     """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
+
     queryset = Post.objects.annotate(
         likes_count=Count('likes', distinct=True),
         comments_count=Count('comment', distinct=True)
@@ -45,14 +51,32 @@ class PostList(generics.ListCreateAPIView):
         'likes__created_at',
     ]
 
-    def perform_create(self, serializer):
-        category_id = self.request.data.get('category')
-        category = get_object_or_404(Category, id=category_id)
-        serializer.save(owner=self.request.user, category=category)
+    def get(self,request):
+        queryset=Post.objects.all()
+        postSerializer=self.get_serializer(queryset,many=True)
+        return Response(postSerializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+    
+        try:
+            user=request.user
+            title=request.data['title']
+            image=request.data['image']
+            content=request.data['content']
+            category= request.data['category']
+            category_id=Category.objects.get(slug=category)
+            post=Post.objects.create(owner=user,title=title,content=content,category=category_id,image=image)
+            post.save()
+            return Response({'message': 'Successfully inserted the post to Database'}, status=status.HTTP_200_OK)
+
+
+        except Exception as e:
+            return Response({"error":"Something went wrong"},status=status.HTTP_400_BAD_REQUEST)
 
     def perform_update(self, serializer):
         category = get_object_or_404(Category, id=self.request.data.get('category'))
         serializer.save(category=category)
+
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
