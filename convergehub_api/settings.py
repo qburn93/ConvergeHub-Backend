99300557008394d14373d32768f38e718d77da45
Bogdan <bogdan.gugu93@gmail.com>
2023-03-26 23:19:12 +0000

    Removed allow all origins from settings file

--- convergehub_api/settings.py
+++ convergehub_api/settings.py
@@ -125,7 +125,7 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'convergehub_api.urls'
