3835bc9cf47bb99be1df654e85e15951a19200ba
Bogdan <bogdan.gugu93@gmail.com>
2023-03-26 22:29:08 +0000

    Forgot migration

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
-from converge_api.permissions import IsOwnerOrReadOnly
+from convergehub_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
