6c0fe38053c8158f51f49202949b3f8f1d3e6dd4
Bogdan <bogdan.gugu93@gmail.com>
2023-03-26 21:51:08 +0000

    Updated req file and backroll previous serializer post because it failed on heroku deployment

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,58 +1,18 @@
 from rest_framework import serializers
 from posts.models import Post
 from likes.models import Like
-from .models import Category
-
+from .models import Post,Category
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
 
-
 class PostSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    like_id = serializers.SerializerMethodField()
-    likes_count = serializers.ReadOnlyField()
-    comments_count = serializers.ReadOnlyField()
-
-    def validate_image(self, value):
-        if value.size > 2 * 1024 * 1024:
-            raise serializers.ValidationError('Image size larger than 2MB!')
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px!'
-            )
-        if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width larger than 4096px!'
-            )
-        return value
-
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
-
-    def get_like_id(self, obj):
-        user = self.context['request'].user
-        if user.is_authenticated:
-            like = Like.objects.filter(
-            owner=user, post=obj
-            ).first()
-            return like.id if like else None
-        return None
+    
     class Meta:
         model = Post
-        fields = fields = [
-            'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter',
-            'like_id', 'likes_count', 'comments_count',
-            
-        ]
+        fields = "__all__"
         depth = 2
 
 
