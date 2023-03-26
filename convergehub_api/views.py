726ff87694b303ca4d1acf61a864b4ae7a5ccd01
Bogdan <bogdan.gugu93@gmail.com>
2023-03-26 23:45:20 +0000

    testing adn debugging to remove 403 auth error

--- convergehub_api/views.py
+++ convergehub_api/views.py
@@ -1,16 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE, JWT_AUTH_SECURE,
 )
 
 
 @api_view()
 def root_route(request):
     return Response({
-        "message": "Welcome To ConvergeHub"
+        "message": "Welcome to my drf API!",
     })
+
 
 # dj-rest-auth logout view fix
 @api_view(['POST'])
@@ -26,7 +26,7 @@ def logout_route(request):
         secure=JWT_AUTH_SECURE,
     )
     response.set_cookie(
-        key=JWT_AUTH_REFRESH_COOKIE,
+        key=JWT_AUTH__REFRESH_COOKIE,
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
