48ae29c273c73dff457b80ccb4f623fceae9e07f
Bogdan <bogdan.gugu93@gmail.com>
2023-03-27 21:05:07 +0000

    broke again and i cant login trying to resolve issue again

--- likes/models.py
+++ likes/models.py
@@ -5,10 +5,10 @@ from posts.models import Post
 
 class Like(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='likes', on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    content = models.TextField()
 
     class Meta:
         ordering = ['-created_at']
