f05c509f27efcdd31d8c70f31f816ec027e1c001
Bogdan <bogdan.gugu93@gmail.com>
2023-03-27 20:08:00 +0000

    updated serializers and urls to maybe fix unlike issue

--- likes/models.py
+++ likes/models.py
@@ -4,16 +4,13 @@ from posts.models import Post
 
 
 class Like(models.Model):
-    """
-    Like model, related to 'owner' and 'post'.
-    'owner' is a User instance and 'post' is a Post instance.
-    'unique_together' makes sure a user can't like the same post twice.
-    """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
         Post, related_name='likes', on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField(default="Default content")
 
     class Meta:
         ordering = ['-created_at']
