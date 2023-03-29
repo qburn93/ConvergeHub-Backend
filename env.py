3db12349c601511502f6d4ee8598cfba4135b803
Bogdan <bogdan.gugu93@gmail.com>
2023-03-29 03:58:58 +0000

    test

--- env.py
+++ env.py
@@ -2,5 +2,5 @@ import os
 
 os.environ['CLOUDINARY_URL'] = "cloudinary://941722266126714:-TMJfrMFYk3cYW6lLC6q7y06HdI@ddvxnbm3h"
 os.environ.setdefault("SECRET_KEY", "123456")
-# os.environ['DEV'] = '1'
+os.environ['DEV'] = '1'
 os.environ['DATABASE_URL'] = "postgres://cgpadmbj:YpRRDgxvbcMamCxOGJxLx7fbytdZZ8l6@mouse.db.elephantsql.com/cgpadmbj"
