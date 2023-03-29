d5397daad2ac1213e571b7506bfc9dccdd64c8e2
Bogdan <bogdan.gugu93@gmail.com>
2023-03-29 12:26:41 +0000

    fix attempt

--- convergehub_api/settings.py
+++ convergehub_api/settings.py
@@ -52,9 +52,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get(
-    'ALLOWED_HOST'), 'localhost', 'https://converhub-backend.herokuapp.com']
-
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+]
 
 # Application definition
 
@@ -156,8 +157,8 @@ if 'DEV' in os.environ:
     }
 else:
     DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
