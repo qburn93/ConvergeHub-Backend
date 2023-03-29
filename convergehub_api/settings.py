06381667accd1a325660d73faeb167f30c052b4c
Bogdan <bogdan.gugu93@gmail.com>
2023-03-29 13:56:58 +0000

    fix attempt

--- convergehub_api/settings.py
+++ convergehub_api/settings.py
@@ -52,7 +52,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['converhub-api.herokuapp.com', 'localhost',]
+ALLOWED_HOSTS = ['https://converhub-backend.herokuapp.com/', 'localhost',]
 
 # Application definition
 
