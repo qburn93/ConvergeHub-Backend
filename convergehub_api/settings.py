a91f2caa825516ef00872b5157d02e88d4744eb2
Bogdan <bogdan.gugu93@gmail.com>
2023-04-04 12:54:45 +0000

    remove allowedhosts comment

--- convergehub_api/settings.py
+++ convergehub_api/settings.py
@@ -52,7 +52,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-# ALLOWED_HOSTS = ['converhub-backend.herokuapp.com', 'localhost',]
+
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 
